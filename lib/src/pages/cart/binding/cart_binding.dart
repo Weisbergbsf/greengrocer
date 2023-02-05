acdf90eb9d8ad06e9798b43a0bc309b765636326
Weisberg Barros Siqueira <weisbergbsf@gmail.com>
2023-02-05 12:54:46 -0300

    feat: GetX - adiciona lazyPut para o CartController

--- lib/src/pages/cart/binding/cart_binding.dart
+++ lib/src/pages/cart/binding/cart_binding.dart
@@ -4,6 +4,9 @@ import 'package:greengrocer/src/pages/cart/controller/cart_controller.dart';
 class CartBinding extends Bindings {
   @override
   void dependencies() {
-    Get.put(CartController());
+    Get.lazyPut(
+      () => CartController(),
+      fenix: true,
+    );
   }
 }

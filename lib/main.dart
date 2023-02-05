0806841c500ecfa94c935b26fea3df3e8fce7d8d
Weisberg Barros Siqueira <weisbergbsf@gmail.com>
2023-02-05 11:51:05 -0300

    feat: define a orientacão do dispositivo como portraitUP

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
 import 'package:greengrocer/src/pages_routes/app_pages.dart';
@@ -8,7 +9,9 @@ void main() {
 
   Get.put(AuthController());
 
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
